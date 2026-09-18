9e34ab33a7b9f69d0449ef9090878006c4b73bd4
kaosure-project <tawinkum.jp@gmail.com>
2026-09-19 00:09:38 +0900

    feat(issuance): add canonical Kao ID issuance module

---
+++ lib/features/kao_id/issuance/application/providers/kao_id_issuance_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../../app/providers.dart';
+import '../../data/datasources/supabase_kao_id_issuance_datasource.dart';
+import '../../data/repositories/kao_id_issuance_repository_impl.dart';
+import '../../domain/entities/kao_id_identity.dart';
+import '../../domain/repositories/kao_id_issuance_repository.dart';
+
+final kaoIdIssuanceDataSourceProvider =
+    Provider<SupabaseKaoIdIssuanceDataSource>(
+  (ref) => SupabaseKaoIdIssuanceDataSource(
+    ref.watch(supabaseClientProvider),
+  ),
+);
+
+final kaoIdIssuanceRepositoryProvider =
+    Provider<KaoIdIssuanceRepository>(
+  (ref) => KaoIdIssuanceRepositoryImpl(
+    ref.watch(kaoIdIssuanceDataSourceProvider),
+  ),
+);
+
+final currentKaoIdIdentityProvider =
+    FutureProvider<KaoIdIdentity?>(
+  (ref) => ref.watch(kaoIdIssuanceRepositoryProvider).getCurrentIdentity(),
+);
